2fddfd4e25f9ff19c79ba325b6d310b32510d6da
lamestllama <eric@eparsonage.com>
2015-08-02 22:47:20 +0930

    Initial Commit

---
+++ conSERN/conSERN.c
@@ -0,0 +1,18 @@
+/*
+ *  conSERN.c
+ *  conSERN
+ *
+ *  Created by Eric Parsonage on 8/2/15.
+ *  Copyright 2015 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "conSERN.h"
+
+void mexFunction(int nlhs, 
+				 mxArray *plhs[], 
+				 int nrhs, 
+				 const mxArray *prhs[])
+{
+mexPrintf("Hello world!\n");
+};
---
+++ conSERN/conSERN.h
@@ -0,0 +1,15 @@
+/*
+ *  conSERN.h
+ *  conSERN
+ *
+ *  Created by Eric Parsonage on 8/2/15.
+ *  Copyright 2015 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "mex.h"
+
+void mexFunction(int nlhs, 
+				 mxArray *plhs[], 
+				 int nrhs, 
+				 const mxArray *prhs[]);
