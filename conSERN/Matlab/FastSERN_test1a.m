2a4ff8bc07e8c89b2ab07674cc36a905125e5b22
Matthew Roughan <matthew.roughan@adelaide.edu.au>
2015-08-12 14:34:30 +0930

    added second test file

--- conSERN/Matlab/FastSERN_test1a.m
+++ conSERN/Matlab/FastSERN_test1a.m
@@ -5,29 +5,26 @@
 % author:  	Matthew Roughan 
 % email:   	matthew.roughan@adelaide.edu.au
 % 
-% test for large s, how the algorithms scale with n
+% test for small s, how the algorithms scale with n
 %
 %
 clear;
 common;
 
-s = 10;
+s = 0.1;
 n_step = 0.25;
-max_n = 8;
-% max_n = 6; 
+max_n = 8.0;
 ns = ceil( 10.^[2:n_step:max_n] );
 max_naive = 10^5;
 max_matlab = 10^4.25;
-% max_naive = 10^4;
-% max_matlab = 10^4;
 k = 1;
 Gs = laplace_trans(g, support, s);  
-q = k ./ ((ns-1) * Gs)
+q = k ./ ((ns-1) * Gs);
  
-runs = 100; % number of simulations to run 
-Ms = [1, 5, 10, 15]; % bucket dimension for large s
+runs = 2; % number of simulations to run
+Ms = [1]; % bucket dimension for small s
  
-% call programs once to make sure they are in memory
+% call program once to make sure it is in memory
 [N, E, d] = waxman_gen(ns(1), s, q(1), i, problem, parameters);
 [x y e from to] = conSERN(DistanceFunction, s, q(1), ns(1), metric, shape, A, connected, Ms(1), threads, algorithm, buffersize, seed);
 
@@ -36,14 +33,14 @@ for i=1:runs
   seed = i;
   for j=1:length(ns)
     n = ns(j);
-          
+      
     if n <= max_matlab
       tic();
       [N, E, d] = waxman_gen(n, s, q(j), seed, problem, parameters);
       times_matlab(i,j) = toc();
       edges_matlab(i,j) = length(d);
     end
-
+  
     if n <= max_naive
       tic();
       [x y n_edges edge_i edge_j] = ...
@@ -75,7 +72,7 @@ E_naive = mean(edges_naive);
 T_matlab = min(times_matlab);
 E_matlab = mean(edges_matlab);
 
-figure(1)
+figure(1) 
 hold off
 semilogx(100,0)
 hold on
@@ -90,21 +87,19 @@ set(gca, 'xtick',  10.^[2:ceil(max_n)]);
 xlabel('n');
 ylabel('time (seconds)');
 
-filename = sprintf('Plots/FastSERN_test1_semilogx.eps', s)
+filename = sprintf('Plots/FastSERN_test1a_semilogx.eps', s)
 print('-depsc', filename);
-
 
 figure(2)
 hold off
 loglog(100,1)
 hold on
 plots = plot(ns, T);
-plots(length(Ms)+1) = plot(ns(find(ns <= max_naive)), T_naive, 'r--');
+plots(length(Ms)+1) = plot(ns(find(ns <= max_naive)), T_naive, 'r--'); 
 plots(length(Ms)+2) = plot(ns(find(ns <= max_matlab)), T_matlab, 'b--');
 legend_str(length(Ms)+1,:) = sprintf('naive ');
 legend_str(length(Ms)+2,:) = sprintf('matlab');
 legend(plots, legend_str, 'location', 'southeast');
-set(gca, 'fontsize', 18);
 set(gca, 'fontsize', 18);
 set(gca, 'xlim',  10.^[2 ceil(max_n)]);
 set(gca, 'xtick',  10.^[2:2:ceil(max_n)]);
@@ -114,5 +109,5 @@ xlabel('n');
 ylabel('time (seconds)');
 grid on
 
-filename = sprintf('Plots/FastSERN_test1_loglog.eps', s)
+filename = sprintf('Plots/FastSERN_test1a_loglog.eps', s)
 print('-depsc', filename);
